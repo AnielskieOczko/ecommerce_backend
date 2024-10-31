abdead6b666bd329daf05868c7665c577d378af4
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-10-31 22:49:19 +0100

    Create EcommerceBackendApplicationTest.java

---
+++ src/test/java/com/rj/ecommerce_backend/EcommerceBackendApplicationTest.java
@@ -0,0 +1,12 @@
+package com.rj.ecommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class EcommerceBackendApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}

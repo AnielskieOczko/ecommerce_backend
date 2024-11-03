801038b9a86de8d757b3faee5646ec87be3047ed
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-11-03 22:32:23 +0100

    securityConfig
    1. removed userDetailsService from SecurityConfig class
    2. created separate class UserDetailsSericeImpl
    3. Remorked SecurityConfig class
    - added DaoAuthProvider
    - added jwt token filter
    - exception handling for unauthorized login
    - added session policy (stateless)
    - cors disabled
    - csrf disabled

---
+++ src/main/java/com/rj/ecommerce_backend/securityconfig/SecurityConfig.java
@@ -0,0 +1,90 @@
+package com.rj.ecommerce_backend.securityconfig;
+
+import com.rj.ecommerce_backend.domain.user.UserDetailsServiceImpl;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    private final AuthTokenFilter authTokenFilter;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+        return authConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                // Disable CORS and CSRF
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+
+                // Configure exception handling
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint((request, response, authException) ->
+                                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized")
+                        )
+                )
+
+                // Set session management to stateless
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                // Configure authorization rules
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/user/**").hasAuthority("ROLE_USER")
+                        .requestMatchers("/public/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .authenticationProvider(authenticationProvider())
+                // Add JWT token filter
+                .addFilterBefore(authTokenFilter, UsernamePasswordAuthenticationFilter.class)
+
+                .build();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(UserRepository userRepository) {
+//        return userEmail -> userRepository.findUserByEmail(userEmail)
+//                .map(UserDetailsImpl::build)
+//                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + userEmail));
+//    }
+}

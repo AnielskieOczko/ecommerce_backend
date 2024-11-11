5c0eef296bae798cda27abe8ce9b1f359f0c5b40
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-11-11 22:22:17 +0100

    Update PhoneNumber.java
    cleared unused imports

--- src/main/java/com/rj/ecommerce_backend/domain/user/valueobject/PhoneNumber.java
+++ src/main/java/com/rj/ecommerce_backend/domain/user/valueobject/PhoneNumber.java
@@ -1,10 +1,6 @@
 package com.rj.ecommerce_backend.domain.user.valueobject;
 
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Embeddable
 public record PhoneNumber(String value) {

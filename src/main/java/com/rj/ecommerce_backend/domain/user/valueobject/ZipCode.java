d611947eaeb4d6569c17921cecd12c8889078406
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-11-11 22:22:30 +0100

    Update ZipCode.java
    cleared unused imports

--- src/main/java/com/rj/ecommerce_backend/domain/user/valueobject/ZipCode.java
+++ src/main/java/com/rj/ecommerce_backend/domain/user/valueobject/ZipCode.java
@@ -2,10 +2,7 @@ package com.rj.ecommerce_backend.domain.user.valueobject;
 
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotEmpty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
 
 @Embeddable
 public record ZipCode(@NotEmpty String value) {

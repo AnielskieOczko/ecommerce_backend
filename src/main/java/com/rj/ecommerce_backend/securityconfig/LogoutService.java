8183be32a95753477474b52e9a9355387b260572
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-11-13 00:17:37 +0100

    Create LogoutService.java
    - created logout service to handle logout
    - jwt token is blacklisted before user is logged out

---
+++ src/main/java/com/rj/ecommerce_backend/securityconfig/LogoutService.java
@@ -0,0 +1,37 @@
+package com.rj.ecommerce_backend.securityconfig;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class LogoutService implements LogoutHandler {
+
+    private final JwtUtils jwtUtils;
+    private final JwtBlacklistService jwtBlacklistService;
+
+    @Override
+    public void logout(HttpServletRequest request,
+                       HttpServletResponse response,
+                       Authentication authentication) {
+
+        String token = jwtUtils.parseJwt(request);
+        String username = token != null ? jwtUtils.getUsernameFromJwtToken(token) : "anonymous";
+
+        if (token != null) {
+            jwtBlacklistService.blacklistToken(token, username);
+            log.info("Token successfully blacklisted for user: {}", username);
+        }
+
+        SecurityContextHolder.clearContext();
+
+    }
+}

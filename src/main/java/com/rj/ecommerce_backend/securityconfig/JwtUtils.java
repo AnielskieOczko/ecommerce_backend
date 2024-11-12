de893afe8a30aa2e0b3c9069b239fdbcfe2ce6ff
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-11-13 00:16:53 +0100

    Update JwtUtils.java
    - changed structure of jwt token:
    1) subject now contains user id instead of username
    2) username is stored in separate claim
    - added new method to get all claims from jwt token
    - reworked method getUsernameFromJwtToken
    - moved parseJwt method from Security config to JwtUtils (it is used atleast in 2 places), previously it was private method
    - changed accessibility for getJwtSecret and GetJwtExpirationMs to be private

--- src/main/java/com/rj/ecommerce_backend/securityconfig/JwtUtils.java
+++ src/main/java/com/rj/ecommerce_backend/securityconfig/JwtUtils.java
@@ -4,11 +4,13 @@ import com.rj.ecommerce_backend.domain.user.UserDetailsImpl;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.security.Key;
 import java.util.Date;
@@ -20,11 +22,11 @@ public class JwtUtils {
 
     private final JwtConfig jwtConfig;
 
-    public String getJwtSecret() {
+    private String getJwtSecret() {
         return jwtConfig.getSecret();
     }
 
-    public int getJwtExpirationMs() {
+    private int getJwtExpirationMs() {
         return jwtConfig.getExpirationMs();
     }
 
@@ -39,11 +41,20 @@ public class JwtUtils {
         log.info(getJwtSecret());
 
         return Jwts.builder()
-                .setSubject(userPrincipal.getUsername())
+                .setSubject(String.valueOf(userPrincipal.getId())) // User ID as subject
+                .claim("username", userPrincipal.getUsername()) // Username as a separate claim
                 .setIssuedAt(new Date())
                 .setExpiration(new Date((new Date()).getTime() + 86400000)) // TODO: change hardcoded value to env variable (getJwtExpiationMs does return 0 - issue to fix)
                 .signWith(key(), SignatureAlgorithm.HS512)
                 .compact();
+    }
+
+    public Claims getTokenClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
     public String getUsernameFromJwtToken(String token) {
@@ -52,7 +63,17 @@ public class JwtUtils {
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
-                .getSubject();
+                .get("username", String.class);
+    }
+
+    public String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
+            return headerAuth.substring(7);
+        }
+
+        return null;
     }
 
     public boolean validateJwtToken(String authToken) {

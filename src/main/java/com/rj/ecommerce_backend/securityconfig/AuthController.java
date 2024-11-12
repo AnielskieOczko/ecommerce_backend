ee752cf2b25ff59996b95405f7d590fc89033af3
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-11-13 00:23:28 +0100

    Update AuthController.java
    - added endpoint for getting user blackListedTokens (later it should be secured for admin access only)

--- src/main/java/com/rj/ecommerce_backend/securityconfig/AuthController.java
+++ src/main/java/com/rj/ecommerce_backend/securityconfig/AuthController.java
@@ -1,11 +1,19 @@
 package com.rj.ecommerce_backend.securityconfig;
 
+import com.rj.ecommerce_backend.securityconfig.dto.AuthResponse;
+import com.rj.ecommerce_backend.securityconfig.dto.JwtResponse;
+import com.rj.ecommerce_backend.securityconfig.dto.LoginRequest;
+import com.rj.ecommerce_backend.securityconfig.dto.TokenInfo;
+import com.rj.ecommerce_backend.securityconfig.exception.UserAuthenticationException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -15,6 +23,8 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private final AuthService authService;
+    private final LogoutService logoutService;
+    private final JwtBlacklistService jwtBlackListedService;
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
@@ -40,4 +50,17 @@ public class AuthController {
                             .build());
         }
     }
+
+    @GetMapping("/user/{userId}/tokens")
+    public ResponseEntity<List<TokenInfo>> getUserTokens(@PathVariable Long userId) {
+        return ResponseEntity.ok(jwtBlackListedService.getUserTokens(userId));
+    }
+
+//    @PostMapping("/logout")
+//    public ResponseEntity<?> logout(HttpServletRequest request) {
+//        // The actual logout handling is done by Spring Security
+//        // This endpoint is just for documentation/clarity
+//        return ResponseEntity.ok()
+//                .body("Logged out successfully");
+//    }
 }

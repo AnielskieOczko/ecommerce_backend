383126e0091e602a15f933ff1e881ef1543374c1
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-11-17 16:17:13 +0100

    Create AdminServiceImpl.java
    admin service for admin actions separation

---
+++ src/main/java/com/rj/ecommerce_backend/domain/user/AdminServiceImpl.java
@@ -0,0 +1,47 @@
+package com.rj.ecommerce_backend.domain.user;
+
+import com.rj.ecommerce_backend.domain.user.dtos.CreateUserRequest;
+import com.rj.ecommerce_backend.domain.user.dtos.UpdateUserRequest;
+import com.rj.ecommerce_backend.domain.user.dtos.UserResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AdminServiceImpl implements AdminService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public List<User> findAllUsers() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<User> findUserByEmail(String email) {
+        return Optional.empty();
+    }
+
+    @Override
+    public UserResponseDto getUserProfileById(Long userId) {
+        return null;
+    }
+
+    @Override
+    public UserResponseDto createUser(CreateUserRequest createUserRequest) {
+        return null;
+    }
+
+    @Override
+    public UserResponseDto updateUser(Long userId, UpdateUserRequest updateUserRequest) {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Long userId) {
+
+    }
+}

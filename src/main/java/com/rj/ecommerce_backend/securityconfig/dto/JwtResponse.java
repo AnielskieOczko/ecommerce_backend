d1055818f9781797767abb9e6e80f8ff96fff762
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-11-18 22:04:18 +0100

    Update JwtResponse.java
    added refreshToken to DTO

--- src/main/java/com/rj/ecommerce_backend/securityconfig/dto/JwtResponse.java
+++ src/main/java/com/rj/ecommerce_backend/securityconfig/dto/JwtResponse.java
@@ -1,5 +1,6 @@
 package com.rj.ecommerce_backend.securityconfig.dto;
 
+import com.rj.ecommerce_backend.securityconfig.RefreshToken;
 import lombok.Data;
 
 import java.util.List;
@@ -7,13 +8,15 @@ import java.util.List;
 @Data
 public class JwtResponse {
     private String token;
+    private String refreshToken;
     private String type = "Bearer";
     private Long id;
     private String email;
     private List<String> roles;
 
-    public JwtResponse(String token, Long id, String email, List<String> roles) {
+    public JwtResponse(String token, String refreshToken, Long id, String email, List<String> roles) {
         this.token = token;
+        this.refreshToken = refreshToken;
         this.id = id;
         this.email = email;
         this.roles = roles;

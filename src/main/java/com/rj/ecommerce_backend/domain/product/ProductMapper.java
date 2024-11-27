061fe89373de9f14adb6a1326b3796caee8f8459
Rafał Jankowski <rafaljankowski7@gmail.com>
2024-11-27 23:19:38 +0100

    Create ProductMapper.java
    To be implemented

---
+++ src/main/java/com/rj/ecommerce_backend/domain/product/ProductMapper.java
@@ -0,0 +1,8 @@
+package com.rj.ecommerce_backend.domain.product;
+
+import com.rj.ecommerce_backend.BaseMapper;
+import com.rj.ecommerce_backend.domain.product.dtos.ProductResponseDTO;
+
+public interface ProductMapper extends BaseMapper<ProductResponseDTO, Product> {
+    // TODO: impl this mapper
+}

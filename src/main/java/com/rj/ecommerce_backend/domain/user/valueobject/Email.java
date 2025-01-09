a56efbd6aeda4be3259fdcc97e999be5a0b343e8
Rafał Jankowski <rafaljankowski7@gmail.com>
2025-01-09 22:59:36 +0100

    Update Email.java
    removed email validation

--- src/main/java/com/rj/ecommerce_backend/domain/user/valueobject/Email.java
+++ src/main/java/com/rj/ecommerce_backend/domain/user/valueobject/Email.java
@@ -4,17 +4,9 @@ import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotEmpty;
 
 @Embeddable
-public record Email(@NotEmpty String value) {
+public record Email(String value) {
     public static Email of(String email) {
-        validateEmail(email);
         return new Email(email);
-    }
-
-    private static void validateEmail(String email) {
-        // Add your email validation logic here
-        if (email == null || email.isEmpty() || !isValidEmail(email)) {
-            throw new IllegalArgumentException("Invalid email address");
-        }
     }
 
     private static boolean isValidEmail(String email) {

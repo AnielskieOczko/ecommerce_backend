5424c8103fc4f1071ad6d826c105551a3cefbf4b
Rafał Jankowski <rafaljankowski7@gmail.com>
2025-01-26 13:28:51 +0100

    rabbitMQ
    - for testig, when user add item to cart - sending request to email serbice to send email.

--- src/main/java/com/rj/ecommerce_backend/domain/user/valueobject/Address.java
+++ src/main/java/com/rj/ecommerce_backend/domain/user/valueobject/Address.java
@@ -2,7 +2,9 @@ package com.rj.ecommerce_backend.domain.user.valueobject;
 
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
 
+@Builder
 @Embeddable
 public record Address(
         String street,
